70de53a22629d361f7133dcfccff482d5fa94166
Rolf Sonderegger <rsonderegger@ethz.ch>
2022-11-16 08:26:36 +0100

    weighting with linear rating curve (Riemann Integral ) added

--- player-extract-data.js
+++ player-extract-data.js
@@ -41,11 +41,14 @@ function parseJSONPlayerInfo(playerData) {
   return playerData;
 }
 
-function reduceToMeanProInterval(playerValues, range) {
+// cm works like this: if you finish games during one day, the new ratings get the timestamp of the current day midnight.
+
+// example
+// two games are finished on May 2nd, one at 14:17 and one at 18:28. Both ratings get the timestamp May 2nd 00:00:00
+
+function reduceToMeanProDay(playerValues, range) {
   let tmpArraySameTimestamp = [];
   let playerRangeValues = [];
-  let playerIntervalValues = {};
-  
   let lastDate = false;
 
   for (let i = 0; i < playerValues.length; i+=1) {
@@ -55,7 +58,7 @@ function reduceToMeanProInterval(playerValues, range) {
       // start to compare with the last timestamp, as soon as we actually have two timestamps -> i > 0
 
       if (playerValues[i-1].x === playerValues[i].x) {
-        // same timestamp, just add to the temporary arry
+        // same timestamp, just add to the temporary array
         tmpArraySameTimestamp.push(playerValues[i]);
       } else {
         // timestamp changed, finish the tmp array (calculate the mean value),
@@ -84,6 +87,77 @@ function reduceToMeanProInterval(playerValues, range) {
       }
     }
   }
+  return playerRangeValues;
+}
+
+function calculateStepsByEdgeCases(ts, olderDateObj, youngerDateObj) {
+  let timeDiffRange = (youngerDateObj.x - olderDateObj.x) / 1000 / 3600 / 24; // time-difference in days
+  
+  if (timeDiffRange < 0) {
+    // swap
+    let tmpOlder = olderDateObj;
+    olderDateObj = youngerDateObj;
+    youngerDateObj = tmpOlder;
+    timeDiffRange = -1 * timeDiffRange;
+  }
+
+  if (ts < olderDateObj.x || ts > youngerDateObj.x) {
+    throw("Timestamp " + (new Date(ts)) + " not within given range: " + (new Date(olderDateObj.x)) + "/" + new Date(youngerDateObj.x) + "!");
+  }
+
+  if (timeDiffRange === 0) {
+    return ((1 * youngerDateObj.y + 1 * olderDateObj.y) / 2);
+  } else {
+    let diffFromOlder = (ts - olderDateObj.x) / 1000 / 3600 / 24;
+    let ratingDiffPerDay = (youngerDateObj.y - olderDateObj.y) / timeDiffRange;
+    return (1 * olderDateObj.y + diffFromOlder * ratingDiffPerDay);
+  }
+}
+
+function calculateMeanBelowRatingCurve(playerVertices, range) {
+  let playerIntervalValues = {};
+  let upperBoundary = new Date(now);
+  upperBoundary.setDate(new Date(now).getDate() + 1); // next midnight (local time)
+  upperBoundary.setHours(0);
+  upperBoundary.setMinutes(0);
+  upperBoundary.setSeconds(0);
+  upperBoundary.setMilliseconds(0);
+  upperBoundary = upperBoundary.getTime();
+  let lowerBoundary = playerVertices[playerVertices.length - 1].x;
+
+  let currentRangeLower = playerVertices[0];
+  let currentRangeUpper = { "x": upperBoundary, "y": playerVertices[0].y }; // current rating 
+
+
+  let options = {
+    year: 'numeric', month: 'numeric', day: 'numeric',
+    hour: 'numeric', minute: 'numeric', second: 'numeric',
+    hour12: false,
+    timeZone: 'Europe/Berlin'
+  };
+
+  let sumOfRatings = 0;
+
+  for (let i = 0, j = 0; i <= range; i+= 1) {
+    // ts for t days
+    let ts = new Date(upperBoundary);
+    ts.setDate(ts.getDate() - i);
+    let ratingStep = calculateStepsByEdgeCases(ts.getTime(), currentRangeLower, currentRangeUpper);
+    //console.log(i + " (" + new Intl.DateTimeFormat('de-DE', options).format(ts) + "): " + ratingStep);
+    sumOfRatings += ratingStep;
+    if (ts.getTime()  === playerVertices[j].x) {
+      currentRangeLower = playerVertices[j + 1];
+      currentRangeUpper = playerVertices[j];
+      j += 1;
+    }
+  }
+  return (sumOfRatings / (range + 1));
+}
+
+function reduceToMeanProInterval(playerRangeValues, range) {
+  let playerIntervalValues = {};
+  
+
   for (let i = 0, j = 0; i < (range * 24); i+=1) {
     // add up count of values for every hour (!) until end of range
  
@@ -102,6 +176,8 @@ function reduceToMeanProInterval(playerValues, range) {
   }
   return playerIntervalValues;
 }
+
+
 
 function calculateMeanValueForRange(playerIntervalValues, range) {
   let playerSum = 0;
@@ -126,11 +202,17 @@ async function main(player, days) {
   await prepareJSONStringFromPlayerTmpFile(player)
   .then ( data => {
     let playerMeanValue = undefined;
+    let meanBelowCurve = undefined;
+    let meanWeightedByTime = undefined;
+
     now = Date.now(); // updating the timestamp as soon as the playerfile is read
 
     data = parseJSONPlayerInfo(data);
-    data = reduceToMeanProInterval(data, meanValueDays)
-    console.log(player + " [" + meanValueDays + " day(s)]: " + calculateMeanValueForRange(data, meanValueDays));
+    data = reduceToMeanProDay(data, meanValueDays);
+    meanBelowCurve = calculateMeanBelowRatingCurve(data, meanValueDays);
+    data = reduceToMeanProInterval(data, meanValueDays);
+    meanWeightedByTime = calculateMeanValueForRange(data, meanValueDays)
+    console.log(player + " [" + meanValueDays + " day(s)]: weighted over time -> " + meanWeightedByTime + " / weighted with linear rating curve -> " + meanBelowCurve);
   })
   .catch ( (e) => {
     console.log("foo" + e);
